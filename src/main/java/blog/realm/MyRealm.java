7b548ea1d5fcc0eedc31b4f35eead70ba5185941
yuzongjian <1306320627@qq.com>
2018-06-09 23:34:54 +0800

    项目脚手架搭建

---
+++ src/main/java/blog/realm/MyRealm.java
@@ -0,0 +1,42 @@
+package blog.realm;
+
+import javax.annotation.Resource;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+/**
+ * 自定义Realm
+ * @author yuzongjian
+ *
+ */
+public class MyRealm extends AuthorizingRealm{
+
+	
+	/**
+	 * 为当限前登录的用户授予角色和权
+	 */
+	@Override
+	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.apache.shiro.realm.AuthenticatingRealm#doGetAuthenticationInfo(org.apache.shiro.authc.AuthenticationToken)
+	 */
+	@Override
+	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken arg0) throws AuthenticationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * 验证当前登录的用户
+	 */
+	
+}
